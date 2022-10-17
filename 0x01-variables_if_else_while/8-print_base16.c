4380d80117e748ed6e9dbcd354a6edc3612647c0
Ikukubreeze <legiondesignsed@gmail.com>
2022-10-16 17:20:58 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,19 +3,22 @@
 /**
  * main - print the letters of the alphabet
  *
- * Description: print the letters of the alphabet in reverse
+ * Description: print the letters of the alphabet except e, q
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i = 122;
+	int i = 0;
 
-	while (i > 96)
+	while (i < 48)
 	{
-		putchar(i);
-		i--;
+		if (i < 10)
+			putchar(i + '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+		i++;
 	}
 	putchar(10);
 
