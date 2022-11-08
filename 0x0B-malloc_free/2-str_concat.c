6f8ea85b2168a35f8b76cf204139b6f94f2d0d11
Ikukubreeze <legiondesignsed@gmail.com>
2022-11-08 11:10:59 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,18 +6,21 @@
  * @s1: Primer string.
  * @s2: Segundo string.
  *
+ * Description: Escriba una función que devuelva un puntero a
+ * un espacio recién asignado en la memoria, que contiene
+ * una copia de la cadena dada como parámetro.
  * Return: The pointer to str.
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int size; 
-	int size2; 
-	int i, j; 
-	char *array; 
+	int size; /* Tamaño del string*/
+	int size2; /* Tamañano del segundo string */
+	int i, j; /* Iteradores de bucle */
+	char *array; /* Salida */
 
 	if (!s1)
-		s1 = ""; 
+		s1 = ""; /* Devuelve un string vacio */
 	if (!s2)
 		s2 = "";
 	for (size = 0; s1[size] != '\0'; size++) /* leng del string */
@@ -28,15 +31,15 @@ char *str_concat(char *s1, char *s2)
 
 	if (!array)
 	{
-		return (NULL);
+		return (NULL); /* Devuelve NULLL si malloc falla */
 	}
 	for (i = 0; i < size; i++)
 	{
-		array[i] = s1[i];
+		array[i] = s1[i]; /* Copiamos string en base a string dado por main */
 	}
 	for (j = 0; j < (size2 + 1); j++)
 	{
-		array[i + j] = s2[j]; 
+		array[i + j] = s2[j]; /* Copiamos string en base a string dado por main */
 	}
-	return (array); 
+	return (array); /* Devolvemos string nuevo */
 }
